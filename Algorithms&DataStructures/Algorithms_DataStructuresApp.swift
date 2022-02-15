82e700c9da5228f8de6ef2f765ceb62c83b63a56
Yasser Farahi <yfarahi@risinggalaxy.com>
2022-02-15 14:49:40 +0100

    Initial Commit

---
+++ Algorithms&DataStructures/Algorithms_DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Algorithms_DataStructuresApp.swift
+//  Algorithms&DataStructures
+//
+//  Created by YASSER FARAHI on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Algorithms_DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Algorithms&DataStructuresUITests/Algorithms_DataStructuresUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Algorithms_DataStructuresUITestsLaunchTests.swift
+//  Algorithms&DataStructuresUITests
+//
+//  Created by YASSER FARAHI on 15/02/2022.
+//
+
+import XCTest
+
+class Algorithms_DataStructuresUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
